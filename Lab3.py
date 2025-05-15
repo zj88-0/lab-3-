660d82ac20f8ddcc59a29b99031bae53b3d7f9c9
Zi Jian <yeezj.24@ichat.sp.edu.sg>
2025-05-15 12:13:01 +0800

    Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -12,7 +12,9 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    if n == 0:
+        arr_result = 0
+    elif n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -35,14 +37,15 @@ def bubble_sort(arr, sorting_order):
                     arr_result = []
     elif n >= 10:
         arr_result = 1
-    elif n == 0:
-        arr_result = 0
+    
+    if not all(isinstance(x, int) for x in arr):
+        arr_result =  2
 
     return arr_result
 
 def main():
     # Driver code to test above
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [64, 34, 25, 12, 22, 11, 90, 89, 90]
 
     # Sort in ascending order
     result = bubble_sort(arr, SORT_ASCENDING)
