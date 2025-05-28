8e32e75e9bb4c6a7e47e0300b1601fffe24009a3
Zi Jian <yeezj.24@ichat.sp.edu.sg>
2025-05-29 02:03:06 +0800

    test lab 2

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -26,3 +26,40 @@ def test_calc_min_max_temperature3():
     min_temp, max_temp = lab2.calc_min_max_temperature(temp_values)
     assert min_temp == expected_min
     assert max_temp == expected_max
+
+def test_calc_average_temperature4():
+    temp_values = [19.6,23.5,36.9,41.2,25.0]
+    expected_average = 29.24
+    average_temp= lab2.calc_average_temperature(temp_values)
+    assert average_temp == expected_average
+def test_calc_average_temperature5():
+    temp_values = [20.0, 20.0, 20.0]
+    expected_average = 20.0
+    average_temp = lab2.calc_average_temperature(temp_values)
+    assert average_temp == expected_average
+def test_calc_average_temperature6():
+    temp_values = [15.0]
+    expected_average = 15.0
+    average_temp = lab2.calc_average_temperature(temp_values)
+    assert average_temp == expected_average
+def test_calc_median_temperature7():
+    temp_values = [19.6, 23.5, 36.9, 41.2, 25.0]
+    expected_median = 25.0
+    median_temp = lab2.calc_median_temperature(temp_values)
+    assert median_temp == expected_median
+
+def test_calc_median_temperature9():
+    temp_values = [20.0, 20.0, 20.0]
+    expected_median = 20.0
+    median_temp = lab2.calc_median_temperature(temp_values)
+    assert median_temp == expected_median
+def test_calc_median_temperature10():
+    temp_values = [15.0]
+    expected_median = 15.0
+    median_temp = lab2.calc_median_temperature(temp_values)
+    assert median_temp == expected_median
+def test_calc_median_temperature8():
+    temp_values = [19.6, 23.5, 36.9, 41.2, 25.0, 20.0]
+    expected_median = 24.25
+    median_temp = lab2.calc_median_temperature(temp_values)
+    assert median_temp == expected_median
